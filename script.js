2640512ff45a3b4738f0292e847aa0a208859305
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-05-10 02:23:02 -0700

    add file

---
+++ script.js
@@ -0,0 +1,6 @@
+$("#save-imagery").click(function () {
+   $("#overlay-success").toggleClass("d-flex d-none");
+});
+$("#save-imagery").click(function () {
+   $("#overlay-danger").toggleClass(" d-none d-flex");
+});
